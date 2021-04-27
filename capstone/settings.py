0e955feb7e72f6c2352c12fd956f781a33f2a81c
Pablo Courault <admin@Mac-mini.local>
2021-04-26 21:56:31 -0300

    cambio navbar por columnas

--- capstone/settings.py
+++ capstone/settings.py
@@ -81,6 +81,10 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "hotel.User"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
