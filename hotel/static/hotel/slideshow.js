4de869622f2d008c73aeff7226ab02c161dabc66
Pablo Courault <admin@Mac-mini.local>
2021-05-05 12:18:13 -0300

    trabajando en slideshow

--- hotel/static/hotel/slideshow.js
+++ hotel/static/hotel/slideshow.js
@@ -34,16 +34,17 @@ function openModal() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
+
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace("active", "");
     }
     slides[slideIndex].style.display = "block";
-    dots[slideIndex].className += " active";
+    dots[slideIndex].className += "active";
   }
 
 });
