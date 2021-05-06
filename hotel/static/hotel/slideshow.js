604fc61a752a37642481bef99fa20249377e7688
Pablo Courault <pcourault@andromeda.intranet.unl>
2021-05-06 16:47:48 -0300

    trabajando en errores puntero de desplazamiento de fotos

--- hotel/static/hotel/slideshow.js
+++ hotel/static/hotel/slideshow.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('img.fotothumbnail').forEach(function(img) {
 
     img.onclick = function() { openModal();
-                               showSlides(img.dataset.fotoid); }  });
+                               slideIndex = img.dataset.fotoid;
+                               currentSlide(slideIndex); }  });
 
   document.querySelector('#close-modal').addEventListener('click', () => closeModal());
 
@@ -11,49 +12,45 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.querySelector('.next').addEventListener('click', () => plusSlides(1));
 
+ // var slideIndex = 1;
+// showSlides(slideIndex);
 
 // Open the Modal
 function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
+    document.getElementById("myModal").style.display = "block"; }
   
   // Close the Modal
   function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
-  
-  /*
-  var slideIndex = 1;
+    document.getElementById("myModal").style.display = "none"; }
 
+// Next/previous controls
+function plusSlides(x) {
+  slideIndex = x + slideIndex;
+  alert(slideIndex);
   showSlides(slideIndex);
-  */
+}
+  
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(n); }
 
-  //  Next/previous controls
-  function plusSlides(n) {
-    showSlides(n);
-  }
-  
-  /*
-  // Thumbnail image controls
-  function currentSlide(n) {
-    slideIndex = n;
-    showSlides(slideIndex);
-  } */
-  
-  function showSlides(slideIndex) {
-    
-    var i;
+
+function showSlides(n) {
+
+    var i = 0;
 
     var slides = document.getElementsByClassName("mySlides");
 
-    if ( slideIndex > 36 ) {slideIndex = 1}
-    if ( slideIndex < 1 ) {slideIndex = 36}
+    if ( n > slides.length ) {slideIndex = 1}
+    if ( n < 1 ) {slideIndex = slide.length}
 
-     for (i = 1; i < 36; i++) {
+     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     } 
 
-    slides[slideIndex].style.display = "block";
+    slides[slideIndex-1].style.display = "block";
+
   }
+
 
 });
